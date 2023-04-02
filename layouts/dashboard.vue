1fe2ad2769740bae2fa2c7c4cef431107d029474
jhonf18 <jhonfred1118@gmail.com>
2023-04-01 22:53:02 -0500

    feat: :lipstick: add animation screen to dashboard section

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative default__layout transition-colors duration-100">
-    <!-- <AnimationScreen></AnimationScreen> -->
+    <AnimationScreen></AnimationScreen>
     <nav class="fixed top-0 z-30 w-full bg-white border-b border-gray-200">
       <div class="px-3 py-3 lg:px-5 lg:pl-3">
         <div class="flex items-center justify-between">
